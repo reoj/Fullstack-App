4bfdec0837241dd8859684ce7b637601bece03f8
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-11-08 10:05:42 -0600

    Fixes the Exchange model for EF

--- Dotnet Backend/Models/Exchange.cs
+++ Dotnet Backend/Models/Exchange.cs
@@ -2,6 +2,7 @@ namespace DotnetBackend.Models
 {
     public class Exchange
     {
+        public Guid Id { get; set; } = new Guid();
         public User sender { get; set; }
         public User reciever { get; set; }
         public string itemName { get; set; } = "Out of Stock";

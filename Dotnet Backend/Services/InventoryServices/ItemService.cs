06a948c134a6b66d98401adcae1608d8e38b2a9d
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-11-09 10:16:26 -0600

    Fixes Bug: Duplicate Items after adding

--- Dotnet Backend/Services/InventoryServices/ItemService.cs
+++ Dotnet Backend/Services/InventoryServices/ItemService.cs
@@ -2,16 +2,19 @@ using DotnetBackend.Models;
 using Microsoft.EntityFrameworkCore;
 using users_items_backend.Context;
 using DotnetBackend.DTOs;
+using DotnetBackend.Services.UserServices;
 
 namespace DotnetBackend.Services.Inventory
 {
     public class ItemService : IInventoryService
     {
         public DataContext _repo { get; set; }
+        public IUserService _uService { get; }
 
         #region Constructor
-        public ItemService(DataContext context)
+        public ItemService(DataContext context, IUserService _uService)
         {
+            this._uService = _uService;
             _repo = context;
         }
         #endregion
@@ -23,6 +26,30 @@ namespace DotnetBackend.Services.Inventory
             async Task<GetItemDTO> rawCreation(object obj)
             {
                 var ci = (CreateIttemDTO)obj;
+
+                // Request the user to the User service
+                var targetUser = await _uService.GetUserRaw(ci.UserId);
+
+                // If this Item already exists in the user's inventory, just increase the quantity
+                if (IsItemInList(await GetItemsOfUser(ci.UserId), ci.Name, ci.Description))
+                {
+                    //Request the item from the user's Inventory                    
+                    var oldItem = ServiceHelper<List<InventoryItem>>.NoNullsAccepted(
+                        targetUser.Items)
+                        .Find(i => i.Name == ci.Name && ci.Description == i.Description);
+                    
+                    // Update the Item
+                    oldItem = ServiceHelper<InventoryItem>.NoNullsAccepted(oldItem);
+                    oldItem.Quantity += ci.Quantity;
+
+                    //Save changes
+                    await _repo.SaveChangesAsync();
+
+                    //Return the DTO to the response handler
+                    return new GetItemDTO(oldItem);
+                }
+
+                // Case: the item is completely new in this inventory. Create Item object
                 InventoryItem nw = new()
                 {
                     Id = Guid.NewGuid(),
@@ -31,9 +58,15 @@ namespace DotnetBackend.Services.Inventory
                     Quantity = ci.Quantity,
                     UserId = ci.UserId,
                 };
-                nw.Owner = await _repo.Users.FirstOrDefaultAsync(u => u.Id == nw.UserId);
+
+                // The Owner of the new Item is the target User
+                nw.Owner = targetUser;
+
+                // Apply changes to the user Inventory
                 await _repo.Items.AddAsync(nw);
                 await _repo.SaveChangesAsync();
+
+                // Return DTO to the Response Handler
                 return new GetItemDTO(nw);
             }
 
@@ -44,11 +77,16 @@ namespace DotnetBackend.Services.Inventory
         {
             async Task<GetItemDTO> rawElimination(object obj)
             {
+                // Get the ID of the item to delete
                 var idn = (Guid)obj;
+
+                // DFind the Item in the Database
                 var old = await _repo.Items.FirstOrDefaultAsync(u => u.Id == idn);
                 old = ServiceHelper<InventoryItem>.NoNullsAccepted(old);
+
                 _repo.Items.Remove(old);
                 await _repo.SaveChangesAsync();
+
                 return new GetItemDTO(old);
             }
             return await ServiceHelper<GetItemDTO>.ActionHandler(rawElimination, id);
@@ -99,5 +137,25 @@ namespace DotnetBackend.Services.Inventory
         }
 
         #endregion
+
+        public async Task<List<InventoryItem>> GetItemsOfUser(int userId)
+        {
+            List<InventoryItem> userInv = new();
+            try
+            {
+                var targetUser = await this._uService.GetUserRaw(userId);
+                userInv = ServiceHelper<List<InventoryItem>>.NoNullsAccepted(targetUser.Items);
+            }
+            catch (Exception err)
+            {
+                throw new NullReferenceException($"Error: {err.Message}");
+            }
+            return userInv;
+        }
+
+        private bool IsItemInList(List<InventoryItem> list, string name, string description)
+        {
+            return list.Exists(i => i.Name == name && i.Description == description);
+        }
     }
 }

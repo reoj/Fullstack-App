42da3642200da6467f04127780c281d86d4fdde9
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-11-09 21:48:58 -0600

    Adds missing signatires in IInventoryService

--- Dotnet Backend/Services/InventoryServices/IInventoryService.cs
+++ Dotnet Backend/Services/InventoryServices/IInventoryService.cs
@@ -13,5 +13,8 @@ namespace DotnetBackend.Services.Inventory
         Task<ServiceResponse<GetItemDTO>> CreateItem(CreateIttemDTO currenItem);
         Task<ServiceResponse<GetItemDTO>> UpdateItem(UpdateItemDTO currenItem);
         Task<ServiceResponse<GetItemDTO>> DeleteItem(Guid id);
+        bool IsItemInList(List<InventoryItem> list, string name, string description);
+        Task<List<InventoryItem>> GetItemsOfUser(int userId);
+        Task<InventoryItem> GetExistingItemRaw(int userid, string name, string description);
     }
 }

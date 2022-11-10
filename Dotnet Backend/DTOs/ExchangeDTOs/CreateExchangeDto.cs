511fd3612fd98a62d4745e76859642882bad198a
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-11-09 21:46:53 -0600

    Adds ctor to CExchangeDTO

--- Dotnet Backend/DTOs/ExchangeDTOs/CreateExchangeDto.cs
+++ Dotnet Backend/DTOs/ExchangeDTOs/CreateExchangeDto.cs
@@ -1,11 +1,22 @@
+using DotnetBackend.Models;
+
 namespace DotnetBackend.DTOs
 {
     public class CreateExchangeDTO
     {
-        public int sender { get; set; }
-        public int reciever { get; set; }
-        public string itemName { get; set; } = "Out of Stock";
-        public string itemDescription { get; set; } = "";
-        public int itemQuantity { get; set; }
+        public int Sender { get; set; }
+        public int Reciever { get; set; }
+        public string ItemName { get; set; } = "Out of Stock";
+        public string ItemDescription { get; set; } = "";
+        public int ItemQuantity { get; set; }
+
+        public CreateExchangeDTO(Exchange ex)
+        {
+            this.Sender = ex.sender.Id;
+            this.Reciever = ex.reciever.Id;
+            this.ItemName = ex.itemName;
+            this.ItemDescription = ex.itemDescription;
+            this.ItemQuantity = ex.itemQuantity;   
+        }
     }
 }

b74429cc06104cb3d822ba7c0f01b94a667763ef
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-11-23 11:49:14 -0600

    Adds usefull documentation to the ServiceResponse

--- Dotnet Backend/Models/ServiceResponse.cs
+++ Dotnet Backend/Models/ServiceResponse.cs
@@ -11,8 +11,17 @@ namespace DotnetBackend.Models
     /// <typeparam name="T">The type of Entity expected in the response body</typeparam>
     public class ServiceResponse<T>
     {
+        /// <summary>
+        /// True if the Response executed as expected, False if an exception ocurred
+        /// </summary>
         public bool Successfull { get; set; } = true;
+        /// <summary>
+        /// The object expected by the user in the response. Null if an exception ocurrs
+        /// </summary>
         public T? Body { get; set; }
+        /// <summary>
+        /// Aditional Information relevant to the state of the response. If an exception occurrs, the exception message can be copied here
+        /// </summary>
         public String Message { get; set; } = "Success";
     }
 }

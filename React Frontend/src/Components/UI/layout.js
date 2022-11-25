f5e584cc4d95b555954bd048afa9ce41d15b5e6d
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-11-25 10:27:31 -0600

    Changes minor  formatting error for components

--- React Frontend/src/Components/UI/layout.js
+++ React Frontend/src/Components/UI/layout.js
@@ -11,7 +11,7 @@ function Layout() {
     <Fragment>
       <Navbar />
       <Provider store={store}>
-      <MainDisplay></MainDisplay>
+      <MainDisplay/>
       <StatusCard/>
       </Provider>
     </Fragment>
